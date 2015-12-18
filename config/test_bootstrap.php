c99c646dae5bc872ecbf6d5af38d822ad3b55d80
rikniitt <riku.niittymaki@gmail.com>
2015-12-18 11:58:05 +0200

    Typo.

--- config/test_bootstrap.php
+++ config/test_bootstrap.php
@@ -2,10 +2,10 @@
 
 // Bootstrap for PHPUnit tests
 
-// Do stuff before intializing $app.
+// Do stuff before initializing $app.
 
 require __DIR__ . '/bootstrap.php';
 
 $app['monolog']->debug('Test bootstrap.');
 
-// Do stuff after intializing $app.
+// Do stuff after initializing $app.
