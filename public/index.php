cd12548400a3b6ce8db07de32d64011d0a2ce9ee
rikniitt <riku.niittymaki@helsinki.fi>
2016-10-27 20:31:17 +0300

    Setup application routes on routes config file.

--- public/index.php
+++ public/index.php
@@ -1,41 +1,6 @@
 <?php
 
 require __DIR__ . '/../config/bootstrap.php';
+require __DIR__ . '/../config/routes.php';
 
-//$authenticationMiddleware = new Legacy\Middleware\TokenAuthentication();
-//$jsonMiddleware = new Legacy\Middleware\JSONRequest();
-//$app->post('/api/some-resource', function () use ($app) {
-//    $receivedData = $app['request']->request->all();
-//    $receivedData['receivedContentType'] = $app['request']->headers->get('Content-Type'); // application/json
-//    $receivedData['receivedAuthenticationToken'] = $app['request']->headers->get('X-Authentication-Token');
-//    $receivedData['info'] = 'This is api endpoint requiring authenctication token on header and accepting a JSON Request Body';
-//    return $app->json($receivedData, 201);
-//})
-//->before(array($authenticationMiddleware, 'before'))
-//->before(array($jsonMiddleware, 'before'));
-
-//$sessionMiddleware = new Legacy\Middleware\Session();
-//$app->get('/login', function () use ($app) {
-//    $app['request']->getSession()->set('userId', 666);
-//    return sprintf('<h1>User #%d currently logged in</h1>', $app['request']->getSession()->get('userId'));
-//})->before(array($sessionMiddleware, 'before'));
-
-//$ipWhitelistMiddleware = new Legacy\Middleware\IPWhitelist();
-//$app->get('/intranetz', function () {
-//    return '<h1>Company wide announcementz here!</h1>';
-//})->before(array($ipWhitelistMiddleware, 'before'));
-
-//$basicAuthMiddleware = new Legacy\Middleware\HttpBasicAuthentication();
-//$app->get('/admin', function () {
-//    return '<h1>Administrator area!</h1>';
-//})->before(array($basicAuthMiddleware, 'before'));
-
-$app->get('/todo/delete/{id}', 'todo.controller:delete');
-$app->get('/todo/edit/{id}', 'todo.controller:edit');
-$app->post('/todo/update/{id}', 'todo.controller:update');
-$app->get('/todo/create', 'todo.controller:create');
-$app->post('/todo/save', 'todo.controller:save');
-$app->get('/todo/{id}', 'todo.controller:show');
-$app->get('/todo', 'todo.controller:index');
-$app->get('/', 'todo.controller:index');
 $app->run();
