12466130a2c09646d4fe95b05e3a73a349fe33f6
Riku <riku@zsystem.fi>
2017-06-08 14:06:07 +0300

    Convert config/services.php to silex 2.

--- config/services.php
+++ config/services.php
@@ -7,19 +7,19 @@
  * @see: https://silex.sensiolabs.org/index.php/doc/2.0/services.html
  */
 
-$app['todo.repository'] = $app->share(function () use ($app) {
+$app['todo.repository'] = function () use ($app) {
     return new Legacy\Database\Repository\Todo($app);
-});
-$app['todo.controller'] = $app->share(function () use ($app) {
+};
+$app['todo.controller'] = function () use ($app) {
     return new Legacy\Controller\Todo($app, $app['todo.repository']);
-});
+};
 
-$app['requestHelper'] = $app->share(function () use ($app) {
+$app['requestHelper'] = function () use ($app) {
     return new Legacy\Library\RequestHelper($app['request']);
-});
-$app['alerts'] = $app->share(function () {
+};
+$app['alerts'] = function () {
     return new Legacy\Library\Alerts\Container();
-});
-$app['flash.alerts'] = $app->share(function () use ($app) {
+};
+$app['flash.alerts'] = function () use ($app) {
     return new Legacy\Library\Alerts\FlashContainer($app['request']->getSession());
-});
+};
