f90a876b088c31a60d9c7e89d7bc3ce0de288e2e
Riku Mäki <riku.niittymaki@gmail.com>
2018-09-18 22:43:10 +0300

    Added resolve method to Legacy\Application

--- config/services.php
+++ config/services.php
@@ -4,7 +4,20 @@
  * Do all your application service bindings
  * to $app here.
  *
+ * function() creates singleton and
+ * $app->factory() returns new instance every time.
+ *
  * @see: https://silex.sensiolabs.org/index.php/doc/2.0/services.html
+ */
+
+$app[Legacy\Application::class] = function () use ($app) {
+    return $app;
+};
+
+/**
+ * Use $app->resolve('Class\Path') to make container instantiate
+ * object from implicitly bind classes.
+ * @see: Legacy\Application::resolve
  */
 
 $app['todo.repository'] = function () use ($app) {
