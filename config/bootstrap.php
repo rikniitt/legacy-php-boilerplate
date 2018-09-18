b2ad140970af227429959f0a97a8fe5dd71be4ed
Riku Mäki <riku.niittymaki@gmail.com>
2018-09-18 23:09:38 +0300

    Code style

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -103,7 +103,6 @@ $app->register(new Silex\Provider\ServiceControllerServiceProvider());
 // or register custom controller provider. Supports Controller@method route callbacks
 //$app->register(new Legacy\Library\Silex\ServiceControllerServiceProvider());
 
-
 // Register silex session provider
 $app->register(new Silex\Provider\SessionServiceProvider());
 
